f0f336f8a204abe9f0c3114259e8327437db9fac
akashkhot12 <akashkhot03@gmail.com>
2024-01-12 10:06:54 +0530

    formated documents

--- ekart/src/app/product-list/product-list.component.ts
+++ ekart/src/app/product-list/product-list.component.ts
@@ -6,28 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  addToCart:number= 0
+  addToCart: number = 0
   product = {
     name: 'Iphone X',
-    price : 789,
-    color:'black',
-    discount:8.5,
-    inStoke : 10,
-    pImage : '../../assets/image/iphon.png'
+    price: 789,
+    color: 'black',
+    discount: 8.5,
+    inStoke: 10,
+    pImage: '../../assets/image/iphon.png'
   }
 
-  getDiscountPrice(){
-   return this.product.price-(this.product.price *this.product.discount/100)
+  getDiscountPrice() {
+    return this.product.price - (this.product.price * this.product.discount / 100)
   }
 
-  decrementCartValue(){
-    if (this.addToCart>0) {
+  decrementCartValue() {
+    if (this.addToCart > 0) {
       this.addToCart--
     }
-    
+
   }
-  incrementCartValue(){
-    if (this.addToCart<this.product.inStoke) {
+  incrementCartValue() {
+    if (this.addToCart < this.product.inStoke) {
       this.addToCart++
     }
   }
