02dde2f9cd507a90fa65886f78fcba37d5caed4f
akashkhot12 <akashkhot03@gmail.com>
2024-01-12 11:35:10 +0530

    adding components

--- ekart/src/app/app.module.ts
+++ ekart/src/app/app.module.ts
@@ -7,9 +7,8 @@ import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { SerchComponent } from './product-list/serch/serch.component';
-
+import { containerComponent } from './container/container.component';;
+import { SerchComponent } from './container/search/serch.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -19,7 +18,7 @@ import { FormsModule } from '@angular/forms';
     TopHeaderComponent,
     TopMenuComponent,
     MainMenuComponent,
-    ProductListComponent,
+    containerComponent,
     SerchComponent
   ],
   imports: [
