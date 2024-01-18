a04cb81cf36425d967fe54cb0e71a203a817e042
akashkhot12 <akashkhot03@gmail.com>
2024-01-18 10:36:50 +0530

    if else condotion applying on code

---
+++ learning/src/app/if-else/if-else.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-if-else',
+  templateUrl: './if-else.component.html',
+  styleUrl: './if-else.component.css'
+})
+export class IfElseComponent {
+  show= true;
+}
