7be173eb23c966dfa56be2f801efa1ecc278d781
akashkhot12 <akashkhot03@gmail.com>
2024-02-05 12:00:00 +0530

    adding property binding component

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './task/counter/counter.component';
+import { PropertyBindingComponent } from './task/property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule,
