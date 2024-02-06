fb5d9d6c960f99e8f170fcc59a01775e4ae4c7a0
akashkhot12 <akashkhot03@gmail.com>
2024-02-06 10:28:03 +0530

    adding new component

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './task/counter/counter.component';
 import { PropertyBindingComponent } from './task/property-binding/property-binding.component';
+import { ConditionsComponent } from './task/conditions/conditions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
-    PropertyBindingComponent
+    PropertyBindingComponent,
+    ConditionsComponent
   ],
   imports: [
     BrowserModule,
