fadef5f83e52787cc89e1da10a5c8d741154b3b5
akashkhot12 <akashkhot03@gmail.com>
2024-02-06 11:24:15 +0530

    nested loop

--- learning-angular/src/app/task/nested-loop/nested-loop.component.ts
+++ learning-angular/src/app/task/nested-loop/nested-loop.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class NestedLoopComponent {
 
+  usersDetails = [
+    {name:'akash',phone:8989,socialAccounts:['facebook','github']},
+    {name:'vinod',phone:989,socialAccounts:['facebook','github']},
+    {name:'jordan',phone:1989,socialAccounts:['facebook','github']},
+    {name:'stephan',phone:1989,socialAccounts:['facebook','github']}
+  ]
+
 }
