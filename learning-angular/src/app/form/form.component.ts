1a9265df540874c78cc64a990fc6d43368163157
akashkhot12 <akashkhot03@gmail.com>
2024-02-12 13:16:03 +0530

    all work done

--- learning-angular/src/app/form/form.component.ts
+++ learning-angular/src/app/form/form.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
-
+  showContent(item:any){
+    console.log(item.value);
+    
+  }
 }
