8976a7c49e621631fb0e0a4b97d32324ed26a83c
akashkhot12 <akashkhot03@gmail.com>
2024-03-07 11:06:15 +0530

    adding functions

--- Code-step-by-step/src/app/app.module.ts
+++ Code-step-by-step/src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { LoginComponent } from './user-auth/login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
